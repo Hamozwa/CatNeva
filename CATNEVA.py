10e0d75cbde41f4005243b2954a302c1546ee589
Hamozwa <iamhanimoussa@gmail.com>
2024-05-18 12:51:19 +0100

    Added expandable logical event process

--- CATNEVA.py
+++ CATNEVA.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import time
 
+#Create image groupings
+idle = [0,1]
+gonnasleep = [2,3,4,5,6,7,8]
+
 
 # Create window
 root = tk.Tk()
@@ -9,8 +13,23 @@ root.geometry("150x150+1300+700")
 root.attributes('-topmost', True) 
 
 # Load images
-image_paths = ["images/Idle1.png", "images/Idle2.png"]
-images = [ImageTk.PhotoImage(Image.open(path)) for path in image_paths]
+image_paths = [
+    "images/Idle1.png",
+    "images/Idle2.png",
+    "images/gonnasleep1.png",
+    "images/gonnasleep2.png",
+    "images/gonnasleep3.png",
+    "images/gonnasleep4.png",
+    "images/gonnasleep5.png",
+    "images/gonnasleep6.png",
+    "images/gonnasleep7.png",
+]
+
+images = []
+for path in image_paths:
+    img = Image.open(path)
+    images.append(ImageTk.PhotoImage(img))
+
 
 # Set background transparent
 root.config(highlightbackground='green')
@@ -19,12 +38,27 @@ root.wm_attributes('-transparentcolor','green')
 
 # Create label
 label = tk.Label(root, bd=0, bg='green')
+label.config(width=150, height=150)
 label.pack()
 
-def update_image(index):
-    label.config(image=images[index])
-    root.after(1000, update_image, (index + 1) % len(images))
+#Decide next event
+def event_decide(last_event):
+    match last_event[0]:
+        case 0:
+            return gonnasleep
+        case 2:
+            return idle
+
+#Run event
+def event_run(event):
+
+    for frame in event:
+        label.config(image=images[frame])
+        root.update()
+        time.sleep(1)
+
+    event_run(event_decide(event))
     
 # Start main loop
-update_image(0)
+event_run(idle)
 root.mainloop()
